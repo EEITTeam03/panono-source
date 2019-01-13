fdff8523557a298326653533f053c68bc8a717ad
洪英皓 <hauhauster@gmail.com>
2019-01-13 13:42:31 +0800

    modified router modified Home.vue

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Home from 'views/Home.vue';
 
 Vue.use(Router);
 
@@ -8,7 +9,7 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
+      path: '/:id?',
       name: 'home',
       component: Home,
     },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,35 @@
 <template>
     <div>
         <loading :active.sync="isLoading" ></loading>
+        <scene v-if="activeLink" />
+        <thumbnails v-if="!isLoading"/>
     </div>
 </template>
 <script>
+import { mapGetters } from 'vuex';
+import Thumbnails from 'components/Thumbnails.vue';
+import Scene from 'components/Scene.vue';
+
 export default {
-  name: 'Home',
+  name: 'home',
   methods: {
     init() {
-      this.$http.get().then((response) => {
-          
-      });
+      if (this.$route.params.id) {
+        this.$store.dispatch('getData', this.$route.params.id);
+      } else {
+        this.$store.dispatch('getData', 'yocico');
+      }
     },
   },
   computed: {
-    ...mapGetters(['isLoading']),
+    ...mapGetters(['isLoading', 'doneData', 'activeLink']),
+  },
+  components: {
+    Thumbnails,
+    Scene,
   },
   created() {
     this.init();
   },
-}
+};
 </script>
