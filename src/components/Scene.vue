f76974792d43646dcf5f00d7a8a60132a2ac84e9
洪英皓 <hauhauster@gmail.com>
2019-01-16 23:15:31 +0800

    modified loading

--- src/components/Scene.vue
+++ src/components/Scene.vue
@@ -2,9 +2,11 @@
     <div>
         <a-scene>
           <a-assets timeout="60000">
-            <img :id="activeId" :src="activeLink" crossorigin="anonymous">
+            <img id="origin" :src="activeLink" crossorigin="anonymous">
+            <img id="preview" :src="previewLink" crossorigin="anonymous">
           </a-assets>
           <a-sky id="image-360" src=""
+          :animation__rotate="rotateIn"
           animation__fadein="startEvents:fadein;
           property:material.opacity;from:0.5;to:1;dur:1000;"
           animation__fadeout="startEvents:fadeout;
@@ -19,33 +21,43 @@ import { mapGetters } from 'vuex';
 export default {
   name: 'Scene',
   methods: {
-    askyLoading() {
-      // 監聽 a-sky 是否 load 完
-      document.querySelector('#image-360').addEventListener('materialtextureloaded', () => {
-        // 取消 loading
-        this.$store.dispatch('updateLoading', false);
-        document.querySelector('#image-360').setAttribute('rotation', this.getRotation);
-        // 加入漸進效果
-        document.querySelector('#image-360').emit('fadein');
+    imgLoading() {
+      document.querySelector('#origin').addEventListener('load', () => {
+        console.log('start loading origin');
+        document.querySelector('#image-360').setAttribute('src', '#origin');
+        console.log('end loading origin');
       });
     },
-    imgLoading() {
-      document.querySelector('a-assets > img').addEventListener('load', () => {
-        console.log('start loading');
-        this.$store.dispatch('updateLoading', false);
-        document.querySelector('#image-360').setAttribute('src', `#${this.activeId}`);
+    previewLoading() {
+      document.querySelector('#preview').addEventListener('load', () => {
+        console.log('start loading privew');
+        document.querySelector('#image-360').setAttribute('position', this.getPosition);
+        document.querySelector('#image-360').setAttribute('src', '#preview');
+        if (document.querySelector('#origin').complete) {
+          document.querySelector('#image-360').setAttribute('src', '#origin');
+        }
         document.querySelector('#image-360').setAttribute('rotation', this.getRotation);
-        document.querySelector('#image-360').emit('fadein');
-        console.log('end loading');
+        console.log('end loading privew');
       });
+    },
+    firstLoading() {
+      // 第一張圖會滑進來
+      document.querySelector('#image-360').addEventListener('materialtextureloaded', () => {
+        this.$store.dispatch('updateLoading', false);
+        document.querySelector('#image-360').emit('rotatein');
+      }, { once: true });
     },
   },
   computed: {
-    ...mapGetters(['activeLink', 'getRotation', 'activeId']),
+    ...mapGetters(['activeLink', 'getRotation', 'previewLink', 'getPosition']),
+    rotateIn() {
+      return `startEvents:rotatein;property:rotation;from:90 0 0;to:${this.getRotation};dur:1000;`;
+    },
   },
   mounted() {
-    // this.askyLoading();
+    this.previewLoading();
     this.imgLoading();
+    this.firstLoading();
   },
 };
 </script>
